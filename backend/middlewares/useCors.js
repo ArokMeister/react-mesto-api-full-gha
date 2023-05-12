6b4776c659088af647b09ab981ef968aeaa8a38f
Denis Shiryaev/ <arokmeister@yandex.ru/>
2023-05-12 23:54:03 +0300

    fix DDos fetch

--- backend/middlewares/useCors.js
+++ backend/middlewares/useCors.js
@@ -1,7 +1,8 @@
 const allowedCors = [
   'https://arokmeister.mesto.nomoredomains.monster',
   'http://arokmeister.mesto.nomoredomains.monster',
-  'http://localhost',
+  'http://localhost:3000',
+  'http://localhost:3001',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
